a803021db567c1e8473cf9b544d128f8c391236d
Belinda Shan <belindashan126@hotmail.com>
2023-10-17 10:21:33 +1100

    updated with base code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./index";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="min-h-screen bg-gray-100">
+      <header className="bg-blue-600 text-white p-4">
+        <h1 className="text-2xl font-semibold">DatingApp</h1>
       </header>
+
+      <section className="p-4">
+        <h2 className="text-xl font-bold mb-4">Features</h2>
+        <p>Discover new people in your city.</p>
+        <p>Safe and secure platform.</p>
+        <p>Find matches based on hobbies and passions.</p>
+      </section>
+
+      <section className="bg-blue-50 p-4">
+        <h2 className="text-xl font-bold mb-4">Join us now!</h2>
+        <button className="bg-blue-600 text-white px-4 py-2 rounded">
+          Sign Up
+        </button>
+      </section>
     </div>
   );
 }
