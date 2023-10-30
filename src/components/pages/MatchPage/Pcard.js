463502d63b30c6789d552b42f185bca3e4db93c1
Belinda Shan <belindashan126@hotmail.com>
2023-10-30 20:44:21 +1100

    used data fetch from the backend server

--- src/components/pages/MatchPage/Pcard.js
+++ src/components/pages/MatchPage/Pcard.js
@@ -2,16 +2,16 @@ import React from "react";
 import { TERipple } from "tw-elements-react";
 import { Button } from "../../UI/Button";
 
-export const Pcard = ({ data, onClick }): JSX.Element => {
-  // console.log(data);
+export const Pcard = (props): JSX.Element => {
+  console.log(props);
   return (
     <div
       className="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]"
-      onClick={onClick}
+      onClick={props.onClick}
     >
       <TERipple>
         <div className="relative overflow-hidden bg-cover bg-no-repeat">
-          <img className="rounded-t-lg" src={data.url} alt="" />
+          <img className="rounded-t-lg" src={props.data.url} alt="" />
           <a href="#!">
             <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsla(0,0%,98%,0.15)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
           </a>
@@ -20,14 +20,14 @@ export const Pcard = ({ data, onClick }): JSX.Element => {
 
       <div className="p-4">
         <h5 className="mb-4 text-xl font-medium leading-tight text-neutral-800">
-          {data.name}
+          {props.data.name}
         </h5>
         <ul>
           <li className="mb-4 text-base text-neutral-600">
-            {data.age}, {data.gender}, {data.city}
+            {props.data.age}, {props.data.gender}, {props.data.city}
           </li>
           <li className="mb-4 text-base text-neutral-600">
-            {data.passion.map((p) => p).join(", ")}
+            {props.data.passion.map((p) => p).join(", ")}
           </li>
         </ul>
 
