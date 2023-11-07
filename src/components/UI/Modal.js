7e3462abcfdb3373e624c8da873f5be340bf5971
Belinda Shan <belindashan126@hotmail.com>
2023-11-07 19:17:50 +1100

    modified the profile paged

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -5,12 +5,14 @@ export const Modal = ({ show, onClose, children }) => {
   if (!show) return null;
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center">
+    <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto">
       <div
         className="absolute inset-0 bg-black opacity-60"
         onClick={onClose}
       ></div>
-      <div className="bg-white pt-5 pb-5 rounded-3xl shadow-lg z-10">{children}</div>
+      <div className="bg-white pt-5 pb-5 rounded-3xl shadow-lg z-10">
+        {children}
+      </div>
     </div>
   );
 };
--- src/components/pages/ProfilePage/Profile.js
+++ src/components/pages/ProfilePage/Profile.js
@@ -3,9 +3,9 @@ import { Button } from "../../UI/Button";
 
 export const Profile = ({ profileData, closeProfileModal }) => {
   return (
-    <div className="bg-gray-100 p-4 md:p-8 min-h-md overflow-y-auto">
+    <div className="bg-gray-100 p-4 md:p-8">
       {/* First Photo Card */}
-      <div className="max-w-md p-6 rounded-xl shadow-lg mb-6 hover:scale-105 transition-transform duration-300">
+      <div className="max-w-md p-6 rounded-xl shadow-lg  mb-6 hover:scale-105 transition-transform duration-300">
         <img
           src={profileData.photo_url}
           className="h-auto max-w-full rounded-lg"
@@ -17,7 +17,7 @@ export const Profile = ({ profileData, closeProfileModal }) => {
       <div className="p-4 max-w-md bg-white rounded-xl shadow-lg mb-6 hover:scale-105 transition-transform duration-300">
         <h1 className="text-xl font-semibold mb-1">👋 {profileData.name}</h1>
         <h2 className="mb-1">
-          {profileData.age}, {profileData.gender}, {profileData.city}
+          {profileData.age}, {profileData.gender}, {profileData.state}
         </h2>
         <p className="text-gray-600">Bio: {profileData.bio}</p>
       </div>
@@ -25,10 +25,20 @@ export const Profile = ({ profileData, closeProfileModal }) => {
       {/* Passions Card */}
       <div className="p-4 bg-white max-w-md rounded-xl shadow-lg mb-6 hover:scale-105 transition-transform duration-300">
         <h2 className="text-lg text-gray-600 mb-2">🔥 Passions</h2>
-        <ul className="pl-5 list-none">
-          {profileData.passion.map((passion) => (
-            <li>{passion}</li>
-          ))}
+        <div>{profileData.passion.map((p) => p).join(", ")}</div>
+      </div>
+
+      {/* Show me Card */}
+      <div className="p-4 max-w-md bg-white rounded-xl shadow-lg mb-6 hover:scale-105 transition-transform duration-300">
+        <h2 className="text-lg text-gray-600 mb-2">👀 Looking For</h2>
+        <ul className="mb-1">
+          <li>Gender: {profileData.gender}</li>
+          <li>
+            Age: {profileData.age_preference.min} -{" "}
+            {profileData.age_preference.max}
+          </li>
+          <li>State: {profileData.state}</li>
+          <li>Relationship: {profileData.relationship}</li>
         </ul>
       </div>
 
