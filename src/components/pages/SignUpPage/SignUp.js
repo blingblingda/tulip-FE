24298ce322e82705c7d3d68f6060c9f7d900f230
Sourabh <sourabhbeniwal@outlook.com>
2023-11-15 11:27:26 +1100

    fixed signup input

--- src/components/pages/SignUpPage/SignUp.js
+++ src/components/pages/SignUpPage/SignUp.js
@@ -133,10 +133,10 @@ export const SignUp = () => {
               </div>
               {/* Confirm Password Input */}
               <div>
-                <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
+                <label htmlFor="password-confirm" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
                 <input
                   type="password"
-                  id="confirm-password"
+                  id="password-confirm"
                   value={confirmPassword}
                   onChange={handleRegister}
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
